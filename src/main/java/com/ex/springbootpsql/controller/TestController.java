9184196f18d782200c90d43112d5c8e562a7a425
sdayeon <dayeonvely@gmail.com>
2023-04-18 16:48:45 +0900

    config 정리

--- src/main/java/com/ex/springbootpsql/controller/TestController.java
+++ src/main/java/com/ex/springbootpsql/controller/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 @Slf4j
